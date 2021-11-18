ac332f7e2eb32c73999c20ed3c0960b298599713
ChanoPark <koc081900@korea.ac.kr>
2021-11-19 06:02:22 +0900

    Add my_settings.py

---
+++ my_settings.py
@@ -0,0 +1,27 @@
+"""    #로컬에서 사용할 때!!
+DATABASES = {
+    'default' : {
+        'ENGINE' : 'django.db.backends.mysql',
+        'NAME' : 'capstone',
+        'USER' : 'root',
+        'PASSWORD' : '451200',
+        'HOST' : '127.0.0.1',
+        'PORT' : '3306',
+    }
+}
+"""
+DATABASES = {
+    'default' : {
+        'ENGINE' : 'django.db.backends.mysql',
+        'NAME' : 'capstone',
+        'USER' : 'admin',
+        'PASSWORD' : '77229680',
+        'HOST' : 'capstone-db.ctghf43fhth2.ap-northeast-2.rds.amazonaws.com',
+        'PORT' : '3306',
+        'OPTIONS' : {
+            'init_command' : 'SET sql_mode="STRICT_TRANS_TABLES"'
+        }
+    }
+}
+
+SECRET_KEY = 'django-insecure-=0=f$aunail)fq3u(8l=4+dcrvlzt6+59u-sbm#)ej^#i2=1cw'
